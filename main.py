004c4a885c18ab207e7fcfe94de76e294d779bf7
Mykola Mazur <mykola1987@gmail.com>
2021-02-24 08:22:16 +0000

    applay mor effective vay to split    one string(message_decoded) in to sentence with separated words

--- main.py
+++ main.py
@@ -2,33 +2,27 @@ alphabet = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
 message = "dfc aruw fsti gr vjtwhr wznj? vmph otis! cbx swv jipreneo uhllj kpi rahjib eg fjdkwkedhmp!"
 key = "friends"
 message_splited = message.split(" ")
-message_stripped = message.replace('!', '').replace('?', '')
+message_stripped = message.replace('!', '').replace('?', '') # eliminate non leters characters from the message
 message_no_space = message_stripped.replace(' ', '')  # make all words in message be  en one long string with no " "
 key_message = key * int(len(message)/(len(key))) + key[0:int(len(message) % len(key))]
 
 def Vigenère_decoder(message, key):
   message_decoded = ""
   message_decoded_sp = ""
-  spaces_location = []
-  spaces_location_new = []
-  #   message_decoded
+  j = 0
+# -------------- message_decoding
   for i in range(len(message_no_space)):
     if message_no_space[i] in alphabet:
       message_decoded = message_decoded + alphabet[(alphabet.find(message_no_space[i]) - alphabet.find(key_message[i]))]
     if ((message_no_space[i] == '!') or (message_no_space[i] == '.') or (message_no_space[i] == '?')):
       message_decoded += message_no_space[i]
-  #  spaces_location
-  for i in range(len(message_stripped)):
-    if message_stripped[i] == " ":
-      spaces_location += [i] 
-  #  spaces_location_new
-  for i in range(len(spaces_location)):
-    spaces_location_new += [spaces_location[i] - i]
-  spaces_location_new = [0] + spaces_location_new + [len(message_no_space)]
-  # message_decoded_sp = message decoded and splited
-  for i in range(len(message_splited)):
-        message_decoded_sp +=  message_decoded[spaces_location_new[i]:spaces_location_new[i +1]] + " "
-        
-        
+# -------------- decoded string splits in to words
+  for i in range(len(message)):
+    if message[i] in alphabet:
+      j += 1           # set a separate counter for this part of for loop so it not missmach when i meets not leter character in message
+      message_decoded_sp += message_decoded[j - 1]
+    if (message[i] in alphabet) == False:
+      message_decoded_sp += message[i]
+           
   return (message_decoded_sp)
 print(Vigenère_decoder(message, key))
