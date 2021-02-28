e04cb4c96e76c182b727546681d38ed3c59b0db3
Mykola Mazur <mykola1987@gmail.com>
2021-02-28 22:31:09 +0000

    + calculate a passubility

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import random
+import math 
 
-num_people_in_room = 40
+num_people_in_room = 23
 birthday_mach_list = {}
 birthday_list1 = {}
 month = ""
-
+counter = 0
 #===  Simulate situation
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 for i in range(1,num_people_in_room + 1):
@@ -31,4 +32,16 @@ for birth_month in birthday_list1:
 t = 0
 for i in birthday_mach_list:
   t += birthday_mach_list[i][1]
+
+print("Here's what our room looks like:\n")
+for month in birthday_list1:
+  for day in range(len(birthday_list1[month])):
+    counter += 1
+    print("Person {i} 's birthday is {month} {date}".format(i = counter, date = birthday_list1[month][day], month = month ))
+print("\n")
 print("In our simulation, there is ", t,   " people have the same birthdays. \non this dates:",birthday_mach_list)
+# === clculate  chanse
+r= 0
+for i in range(2,num_people_in_room + 1):
+  r =round((1 - math.factorial(365)/((365**num_people_in_room)*math.factorial(365-num_people_in_room)))*100,2)
+print("The probability that two people in a room of {n} people have the same birthday is nearly {k}%".format(n = num_people_in_room, k = r))
